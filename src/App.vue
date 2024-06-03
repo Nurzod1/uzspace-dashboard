26d829d71e61fa5dc3892116bafebb01c1207286
Акбаржон Ахмаджонов <a.Axmadjonov@ad.local>
2024-06-03 15:11:45 +0500

    Initial commit and add MainSidebar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MainSidebar from '@/components/MainSidebar.vue'
+</script>
+<template>
+  <MainSidebar />
+</template>
+
+<style>
+@media (min-width: 1440px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
