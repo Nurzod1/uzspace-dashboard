50c0110b36e5b1ec2146126dca887c023408d82d
Azamxon Azamov <A.Azamov@ad.local>
2024-06-06 11:08:30 +0500

    add tab in Map view

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,10 +1,22 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
-  </el-tabs>
+  <div class="map">
+    <div class="map-tab">
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="Tashkent" name="first">Tashkent</el-tab-pane>
+        <el-tab-pane label="Samarkand" name="second">Samarkand</el-tab-pane>
+        <el-tab-pane label="Bukhara" name="third">Bukhara</el-tab-pane>
+        <el-tab-pane label="Namangan" name="fourth">Namangan</el-tab-pane>
+        <el-tab-pane label="Andijan" name="five">Andijan</el-tab-pane>
+        <el-tab-pane label="Fergana" name="six">Fergana</el-tab-pane>
+        <el-tab-pane label="Karshi" name="seven">Karshi</el-tab-pane>
+        <el-tab-pane label="Urgench" name="eight">Urgench</el-tab-pane>
+        <el-tab-pane label="Nukus" name="nine">Nukus</el-tab-pane>
+        <el-tab-pane label="Navoi" name="ten">Navoi</el-tab-pane>
+        <el-tab-pane label="Jizzakh" name="twelve">Jizzakh</el-tab-pane>
+        <el-tab-pane label="Termez" name="eleven">Termez</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -18,11 +30,36 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event)
 }
 </script>
-<style>
-.demo-tabs > .el-tabs__content {
+<style lang="scss">
+
+.el-tabs__nav-wrap:after {
+  display: none;
+}
+.el-tabs__nav-scroll {
   padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
+    padding-bottom: 15px;
+}
+
+.el-tabs__item {
+  color: #A4A4A4;
+  padding: 0 8px;
+  &.is-active {
+    color: #000000;
+    font-weight: 600;
+  }
+}
+
+.el-tabs__active-bar {
+  background-color: #000000;
+}
+
+.el-tabs__item:hover {
+    color: #636363;
+    cursor: pointer;
+}
+
+
+
+.map {
 }
 </style>
